5cffb613197169a96514ccf1401e3351605fefc4
Briseida Pagador <bpagador@gmail.com>
2020-06-17 15:09:44 -0700

    write copy function

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
+const { clear } = require('console');
 
-const fs = require('fs').promises;
+const fsPromises = require('fs').promises;
 
-const readAFile = fs.readFile('./package.json', { encoding: 'utf-8' });
+//read a file
+
+const readAFile = fsPromises.readFile('./README.md', { encoding: 'utf-8' });
 
 readAFile
   .then(contents => {
     return console.log(contents);
   });
 
-fs.writeFile('./write-file.txt', 'text file')
+// write a file 
+
+fsPromises.writeFile('./write-file.txt', 'oh I can write whatever I want!')
   .then(() => console.log('DONE!'));
+
+// copy a file 
+// having the hardest time wrapping my head around this one, moving on to copy function 
+
+
+// copy function
+
+const copyFile = (src, dst) => {
+  return fsPromises.readFile(src)
+    .then(result => fsPromises.writeFile(dst, result));
+};
+
+module.exports = {
+  copyFile
+};
+
