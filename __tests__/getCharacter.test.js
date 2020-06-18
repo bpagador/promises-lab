b952e24def56117966a5c084c789a6f3977a3be0
Briseida Pagador <bpagador@gmail.com>
2020-06-17 17:29:32 -0700

    show working getManyCharacters function and test

--- __tests__/getCharacter.test.js
+++ __tests__/getCharacter.test.js
@@ -1,4 +1,4 @@
-const { getCharacter } = require('../lib/getCharacter');
+const { getCharacter, getManyCharacters } = require('../lib/getCharacter');
 
 jest.mock('superagent', () => ({
   get: () => {
@@ -8,21 +8,56 @@ jest.mock('superagent', () => ({
         'name': 'Antenna Morty',
         'status': 'Alive',
         'species': 'Human'
-      }
+      }      
     });
   }
 }));
+// I keep getting an error message when trying to add another object into the body, what's the deal?
 
 describe('functions to get Rick and Morty character(s)', () => {
 
   it('gets one character from the api', () => {
-    return getCharacter(18)
+    return getCharacter(1)
       .then(result => {
         expect(result).toEqual({
           'name': 'Antenna Morty',
           'status': 'Alive',
           'species': 'Human'
         });
+      });
+  });
+
+  it('get many characters from API', () => {
+    return getManyCharacters([1, 2, 3, 4, 5])
+      .then(result => {
+        expect(result).toEqual([
+          {
+            'name': 'Antenna Morty',
+            'status': 'Alive',
+            'species': 'Human'
+          },
+          {
+            'name': 'Antenna Morty',
+            'status': 'Alive',
+            'species': 'Human'
+          },
+          {
+            'name': 'Antenna Morty',
+            'status': 'Alive',
+            'species': 'Human'
+          }
+          ,
+          {
+            'name': 'Antenna Morty',
+            'status': 'Alive',
+            'species': 'Human'
+          },
+          {
+            'name': 'Antenna Morty',
+            'status': 'Alive',
+            'species': 'Human'
+          }
+        ]);
       });
   });
 });
